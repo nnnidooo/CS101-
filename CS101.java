c3ea149f31af619b3a4f628fbe8b6e1d355c719f
nnnidooo <nadi3252@stud.kea.dk>
2023-01-31 12:33:14 +0100

    method 2 og giver 3 et forsøg

--- CS101.java
+++ CS101.java
@@ -1,20 +1,34 @@
 import java.util.Scanner;
 public class CS101 {
     //Methods 1
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //methods 1
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your age");
         int personsAge = sc.nextInt();
-        if (personsAge >= 18){
+        if (personsAge >= 18) {
             System.out.println("You are eligible to vote");
-        }  else if (personsAge < 18){
+        } else if (personsAge < 18) {
             System.out.println("You are not eligible to vote");
         }
     }
 
-    public static void maximumNumber(int first, int second, int third){
-        int max = getMax(2,5,3);
+    //Methods 2
+    public static void maximumNumber(int first, int second, int third) {
+        int max = getMax(2, 5, 3);
         System.out.println(max);
     }
+
+    public static void minimumNumber(int first, int second, int third) {
+        int min = getMax(2, 5, 3);
+        System.out.println(min);
+    }
+
+    public static void evenOrNot(int first){
+        boolean evenNumber = (num % 2 == 0);
+        int personsNum = sc.nextInt();
+        if ()
+    }
 }
+
+
